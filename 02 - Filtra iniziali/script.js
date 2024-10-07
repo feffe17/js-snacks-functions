32e350f1dbe4e05388788af3d5cd109a188c2da9
feffe1702 <fpm.asd@gmail.com>
2024-10-07 14:09:22 +0200

    [DONE] complete ex2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,39 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtro(array, lettera) {
+    let iniziale;
+    let nomiFiltrati = [];
+    for (let i = 0; i < array.length; i++) {
+        iniziale = array[i].charAt(0);
+        if(lettera === iniziale){
+            nomiFiltrati.push(array[i]);
+        }
+        
+    }
+    console.log(nomiFiltrati);
+    
+}
+
+const filtroArrow = (array, lettera) => {
+    let iniziale;
+    let nomiFiltrati = [];
+    for (let i = 0; i < array.length; i++) {
+        iniziale = array[i].charAt(0);
+        if(lettera === iniziale){
+            nomiFiltrati.push(array[i]);
+        }
+        
+    }
+    console.log(nomiFiltrati);
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+filtro(names , "A");
+filtroArrow(names , "L");
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
