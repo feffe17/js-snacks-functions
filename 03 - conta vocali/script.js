f761b62469e2100cfe5ecfba339da96eb3b434f0
feffe1702 <fpm.asd@gmail.com>
2024-10-07 14:16:26 +0200

    [WIP] dichiarata funzione ex3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function contaVocali(string) {
+    const vocali = ["a" , "e" , "i" , "o" , "u"];
+    string.split("");
+    for (let i = 0; i < vocali.length; i++) {
+        
+        console.log( string);
+        
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+contaVocali(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
