5d002f2a0c3fc703dfc239ac1845bcf6a66d5313
feffe1702 <fpm.asd@gmail.com>
2024-10-07 13:59:09 +0200

    [DONE] complete ex 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,32 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali(arr) {
+    let lettere = [];
+    for (let i = 0; i < arr.length; i++) {
+        lettere.push(arr[i].charAt(0));
+        
+        
+    }
+    console.log(lettere);
+}
+
+
+const inizialiArrow = (arr) => {
+    let lettere = [];
+    for (let i = 0; i < arr.length; i++) {
+        lettere.push(arr[i].charAt(0));
+    }
+    console.log(lettere);
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+iniziali(names);
+
+inizialiArrow(names);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
